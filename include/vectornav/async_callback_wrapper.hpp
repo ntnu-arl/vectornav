fb841bea54662619e224c07a583c093ba586adac
Nikhil Khedekar <nikhilvkhedekar@gmail.com>
2022-03-27 23:52:45 +0200

    fixed indent

--- include/vectornav/async_callback_wrapper.hpp
+++ include/vectornav/async_callback_wrapper.hpp
@@ -7,8 +7,8 @@ namespace vectornav
 {
   void callback_wrapper(void* user_data, Packet& p, size_t index)
   {
-    static_cast<VectorNav *>(user_data)->BinaryAsyncMessageCallback(p, index);
+    static_cast<VectorNav*>(user_data)->BinaryAsyncMessageCallback(p, index);
   }
-} // namespace vectornav
+}  // namespace vectornav
 
-#endif // ASYNC_CALLBACK_WRAPPER_HPP_
+#endif  // ASYNC_CALLBACK_WRAPPER_HPP_
